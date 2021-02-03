88d062cd92e4c553d8eecd71a681de855359fded
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2021-02-03 00:13:22 -0600

    Ch2ProgChal2 Completed

--- SummerfieldCh2ProgChal2/SummerfieldCh2ProgChal2/SummerfieldCh2ProgChal2.cpp
+++ SummerfieldCh2ProgChal2/SummerfieldCh2ProgChal2/SummerfieldCh2ProgChal2.cpp
@@ -1,20 +1,15 @@
-// SummerfieldCh2ProgChal2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+double length_feet, width_feet, acres_convert, total_acres;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	length_feet = 869;
+	width_feet = 360;
+	acres_convert = 43560;
+
+	total_acres = (length_feet * width_feet) / acres_convert;
+
+	cout << "TOTAL ACRES: " << total_acres << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
