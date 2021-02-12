3d1e8aee93897cb2794b2573b9dc7d92ba3ebc98
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2021-02-12 02:00:00 -0600

    Cleaning up Ch4ProgChal1

--- SummerfieldCh4ProgChal1/SummerfieldCh4ProgChal1/SummerfieldCh4ProgChal1.cpp
+++ SummerfieldCh4ProgChal1/SummerfieldCh4ProgChal1/SummerfieldCh4ProgChal1.cpp
@@ -14,35 +14,27 @@ int main()
 		if (books == 1)
 		{
 			cout << "You have been awarded 5 points!";
-			points += 5;
 		}
 		else if (books == 2)
 		{
-			points += 15;
 			cout << "You have been awarded 15 points!";
 		}
 		else if (books == 3)
 		{
-			points += 30;
 			cout << "You have been awarded 30 points!";
 		}
 		else if (books >= 4)
 		{
-			points += 50;
 			cout << "You have been awarded 50 points!";
 		}
 		else
 		{
-			cout << "You haven't purchased any books!\nPurchase books to earn points!";
+			cout << "You haven't purchased any books, or you tried inputting something that is not a number.\nPurchase books to earn points!";
 		}
 	}
 	else if (books <= 0)
 	{
 		cout << "You can't enter negative numbers!";
-	}
-	else
-	{
-		cout << "You must enter a number!";
 	}
 
 	return 0;
